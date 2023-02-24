169333ac3a660213c5043a3058f82143fe8ea711
Zer0 <carlito_zombie_gamer@live.com>
2023-02-24 02:28:19 -0400

    Working version

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
+const {Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions} = require("discord.js");
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 const axios = require('axios');
+const config = require("./config.json");
+const prefix = '!';
 
-client.on("message", msg => {
-  if (msg.content.toString() == "/givewaifupls") {
-    const URL = 'https://api.waifu.pics/sfw/waifu';
-    axios
-      .get(URL)
-      .then(response => {
-        console.log(response.data.url);
-        msg.channel.send(response.data.url);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+
+
+client.on("ready", () => {
+
+  console.log("Bot is online")
+  client.user.setActivity("Test kekw");
+
+})
+
+client.on("messageCreate", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  const messageArray = message.content.split(" ");
+  const argument = messageArray.slice(1);
+  const cmd = messageArray[0];
+
+
+  if (command === 'test') {
+    message.channel.send("Holy shit finally");
   }
-  if (msg.content.startsWith("/givewaifuplsnsfw")) {
-    const URL = 'https://api.waifu.pics/nsfw/waifu';
-    
-    try{axios
-      .get(URL)
-      .then(response => {
+
+
+
+
+  if (command === "givewaifupls") {
+    const URL = 'https://api.waifu.pics/sfw/waifu';
+    async function getWaifuURL() {
+      try {
+        const response = await axios.get(URL);
         console.log(response.data.url);
-        msg.reply('Feeling spicy? One sec buddy!');
-        msg.channel.send({
-          files: [{
-            attachment: response.data.url,
-            name: "SPOILER_FILE.jpg"
-          }]
-        });
-      })
-      .catch(error => {
+        message.channel.send(response.data.url);
+      } catch (error) {
         console.log(error);
-      });
+      }
+    }
+    getWaifuURL();
+  }
+
+  if (command === "givewaifuplsnsfw") {
+    const URL = 'https://api.waifu.pics/nsfw/waifu';
+    try {
+      axios
+        .get(URL)
+        .then(response => {
+          console.log(response.data.url);
+          message.reply('Feeling spicy? One sec buddy!');
+          message.channel.send({
+            files: [{
+              attachment: response.data.url,
+              name: "SPOILER_FILE.jpg"
+            }]
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
   }
 })
-client.login(process.env.TOKEN)
-
-
+client.login(config.key);
